6b442e7aa29ddcb6cf439e4bf6de57cb3384c5c5
elerac <maenosuke079@gmail.com>
2021-05-30 16:07:49 +0900

    Create 02_relighting.py

---
+++ 02_relighting.py
@@ -0,0 +1,42 @@
+"""Relighting with the Light Transport Matrix
+"""
+import numpy as np
+import cv2
+from skimage.exposure import adjust_gamma
+import structuredlight as sl
+
+def main():
+    # Image size captured by camera
+    m = 480
+    n = 300
+    cc = 3 # color
+    # Projection pattern size
+    p = 16
+    q = 9
+    dir_name = f"lighttransport_{n}x{m}x{cc}_{q}x{p}"
+    print("Load light_transport_matrix")
+    T = np.load(f"{dir_name}/light_transport_matrix.npy") # (m*n*cc, p*q)
+
+    # Relight pattern
+    img_input = np.zeros((q, p, 3), dtype=np.uint8)
+    img_input[:,       :p//3,   0] = 255 # B
+    img_input[:,   p//3:p*2//3, 1] = 255 # G
+    img_input[:, p*2//3:,       2] = 255 # R
+    
+    print("Calculate relighting")
+    T_reshaped = np.reshape(T, (m*n, cc, p*q)) # (m*n, cc, p*q)
+    imlist_output = []
+    for i in range(cc):
+        P = img_input[..., i].flatten() # (p*q)
+        C = T_reshaped[:, i, :] @ P # (m*n, p*q) x (p*q) = (m*n)
+        img = np.reshape(C, (n, m)) # (n, m)
+        imlist_output.append(img)
+    
+    img_output = cv2.merge(imlist_output) # (n, m, cc)
+    img_output_u8 = adjust_gamma(np.clip(img_output*4.0, 0, 255).astype(np.uint8), gamma=1/1.8)
+    
+    cv2.imwrite(f"{dir_name}/input.png", img_input)
+    cv2.imwrite(f"{dir_name}/output.png",  img_output_u8)
+
+if __name__=="__main__":
+    main()
