c0b2e1610dcaa73acf71eecfdb8b5a2d2ea42506
elerac <maenosuke079@gmail.com>
2021-06-12 22:33:16 +0900

    Update 02_relighting.py

--- 02_relighting.py
+++ 02_relighting.py
@@ -9,13 +9,13 @@ def main():
     # Image size captured by camera
     m = 480
     n = 300
-    cc = 3 # color
+    co = 3 # color
     # Projection pattern size
     p = 16
     q = 9
-    dir_name = f"lighttransport_{n}x{m}x{cc}_{q}x{p}"
+    dir_name = f"lighttransport_{n}x{m}x{co}_{q}x{p}"
     print("Load light_transport_matrix")
-    T = np.load(f"{dir_name}/light_transport_matrix.npy") # (m*n*cc, p*q)
+    T = np.load(f"{dir_name}/light_transport_matrix.npy") # (m*n*co, p*q)
 
     # Relight pattern
     img_input = np.zeros((q, p, 3), dtype=np.uint8)
@@ -24,19 +24,19 @@ def main():
     img_input[:, p*2//3:,       2] = 255 # R
     
     print("Calculate relighting")
-    T_reshaped = np.reshape(T, (m*n, cc, p*q)) # (m*n, cc, p*q)
+    T_reshaped = np.reshape(T, (m*n, co, p*q)) # (m*n, co, p*q)
     imlist_output = []
-    for i in range(cc):
+    for i in range(co):
         P = img_input[..., i].flatten() # (p*q)
         C = T_reshaped[:, i, :] @ P # (m*n, p*q) x (p*q) = (m*n)
         img = np.reshape(C, (n, m)) # (n, m)
         imlist_output.append(img)
     
-    img_output = cv2.merge(imlist_output) # (n, m, cc)
+    img_output = cv2.merge(imlist_output) # (n, m, co)
     img_output_u8 = adjust_gamma(np.clip(img_output*4.0, 0, 255).astype(np.uint8), gamma=1/1.8)
     
     cv2.imwrite(f"{dir_name}/input.png", img_input)
-    cv2.imwrite(f"{dir_name}/output.png",  img_output_u8)
+    cv2.imwrite(f"{dir_name}/output.png", img_output_u8)
 
 if __name__=="__main__":
     main()
