be2b527864f2bb23092955b0ac7d203c14374f21
elerac <maenosuke079@gmail.com>
2021-06-12 22:33:06 +0900

    Update 01_calculate_light_transport.py

--- 01_calculate_light_transport.py
+++ 01_calculate_light_transport.py
@@ -9,24 +9,24 @@ def main():
     # Image size captured by camera
     m = 480
     n = 300
-    cc = 3 # color
+    co = 3 # color
     # Projection pattern size
     p = 16
     q = 9
-    dir_name = f"lighttransport_{n}x{m}x{cc}_{q}x{p}"
+    dir_name = f"lighttransport_{n}x{m}x{co}_{q}x{p}"
 
     code_names = sorted(glob.glob(f"{dir_name}/code*.png"))
     print("The projected patterns")
     print(code_names[:15])
-    imlist_code = [ cv2.imread(name, 0) for name in code_names]
+    imlist_code = [cv2.imread(name, 0) for name in code_names]
     
     observe_names = sorted(glob.glob(dir_name + "/" + "observe*.png"))
     print("The images captured by the camera")
     print(observe_names[:15])
-    imlist_observe = [ cv2.imread(name, 1) for name in observe_names]
+    imlist_observe = [cv2.imread(name, 1) for name in observe_names]
 
     decoder = sl.LightTransport()
-    T = decoder.decode(imlist_code, imlist_observe) # (m*n*cc, p*q)
+    T = decoder.decode(imlist_code, imlist_observe) # (m*n*co, p*q)
     print("Light Transport Matrix")
     print(T.shape)
     print(T.dtype)
